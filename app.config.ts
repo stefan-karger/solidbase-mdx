6f2e8a8b19ee045fbda5cbd1972ab9aa1b62d342
Stefan Karger <stefan.eideloth@gmail.com>
2025-10-06 13:23:16 +0200

    switch to netlify preset

--- app.config.ts
+++ app.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(
     {
       ssr: true,
       server: {
-        preset: "vercel"
+        preset: "netlify"
       },
       vite: {
         plugins: [tailwindcss()]
