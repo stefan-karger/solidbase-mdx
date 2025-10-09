e4f362aaddaf2683716189baef52748108938b33
Stefan Karger <stefan.eideloth@gmail.com>
2025-10-09 15:21:30 +0200

    .

--- src/components/component-preview.tsx
+++ src/components/component-preview.tsx
@@ -1,7 +1,7 @@
 import { type ComponentProps, lazy, splitProps } from "solid-js"
 
 export function ComponentPreview(props: ComponentProps<"div"> & { name: string }) {
-  const [local, others] = splitProps(props, ["name"])
+  const [, others] = splitProps(props, ["name"])
   const Comp = lazy(() => import("~/registry/examples/button-example"))
   return (
     <div
